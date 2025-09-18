b90228dab54f99fd75ab6469d44470c306e8e5e2
ellenhts18 <ellen.soares18@escola.pr.gov.br>
2025-09-18 13:40:09 +0000

    aula6

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const avanca = document.querySelectorAll('.bnt-proximo');
-avanca.forEach (button=>{
-    button.addEventLister(cancelIdleCallback, function(){
-    const atual = document.querySelector('.ativo')
-   const proximoPasso = 'passo-' + this.getAtribite('data-proximo')
-  
-atual,classList.remove('ativo')
-document.getElementById(proximoPasso).classList.add('ativo');
+const avanca = document.querySelectorAll('.btn-proximo');
 
-})
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
 })
